0cbd825fa5fecfba13cf17b77f521a76b75617cd
KarenACC <karen_abi@live.com.mx>
2023-11-08 13:40:29 -0600

    Se añade historial de partidas usando session storage

--- main.js
+++ main.js
@@ -1,32 +1,40 @@
-document.addEventListener('DOMContentLoaded', function() {
-
 let pcTiro;
-let boton = document.getElementById('boton');
-let respuestas= document.querySelectorAll('.mostrar')
-let divJugador= document.getElementById('jugador-eleccion')
+let playerTiro;
+const boton = document.getElementById('boton');
 
-let instruccion= document.getElementById('instruccion');
+// div que aparecerá para mostrar la respuesta de la pc y el resultado
+const respuestas= document.querySelectorAll('.mostrar')
+
+// div donde aparece la imagen de la opcion del jugador
+const divJugador= document.getElementById('jugador-eleccion')
+
+const instruccion= document.getElementById('instruccion');
 
 let imgJugador = document.getElementById('imgJugador')
 let imgPc = document.getElementById('imgPc')
+let resultado = document.getElementById('resultado')
 
-let btnPiedra = document.getElementById('btn-piedra')
-let btnPapel = document.getElementById('btn-papel')
-let btnTijera = document.getElementById('btn-tijera')
+const btnPiedra = document.getElementById('btn-piedra')
+const btnPapel = document.getElementById('btn-papel')
+const btnTijera = document.getElementById('btn-tijera')
 
-let reload = document.getElementById('replay');
+const reload = document.getElementById('replay');
 
-let numAleatorio = Math.round(Math.random()*(1-3)+3);
+let respuesta
+let resultadosHistorial=[]
+let listaHistorial = document.getElementById('listaHistorial')
+
+const numAleatorio = Math.round(Math.random()*(1-3)+3);
 
         switch (numAleatorio) {
             case 1:
-                pcTiro='Piedra'
+                pcTiro='piedra'
                 break;
             case 2:
-                pcTiro='Papel'
+                pcTiro='papel'
                 break;
             case 3:
-                pcTiro='Tijera'
+                pcTiro='tijera'
                 break;
             default:
                 break;
@@ -36,46 +44,14 @@ btnPiedra.addEventListener('click', function(){
     imgJugador.src = './img/piedra.png'
     divJugador.style.display= 'block';
     boton.disabled= false;
-    
-    if (pcTiro== 'Piedra') {
-        imgPc.src = './img/piedra.png'
-        console.log('Empate');
-        resultado.src = './img/empate.png'
-        
-    };
-    if (pcTiro=='Papel') {
-        imgPc.src = './img/papel.png'
-        console.log('Tú pierdes'); 
-        resultado.src = './img/perder.png'
-    };
-    if (pcTiro=='Tijera') {
-        imgPc.src = './img/tijera.png'
-        console.log('Tú ganas'); 
-        resultado.src = './img/ganar.png'
-    }
+    playerTiro= 'piedra'
 });
 
 btnPapel.addEventListener('click', function(){
     imgJugador.src = './img/papel.png'
     divJugador.style.display= 'block';
     boton.disabled= false;
-
-    if (pcTiro== 'Piedra') {
-        console.log('Tú ganas'); 
-        imgPc.src = './img/piedra.png'
-        resultado.src = './img/ganar.png'
-    };
-    if (pcTiro=='Papel') {
-        imgPc.src = './img/papel.png'
-        console.log('Empate'); 
-        resultado.src = './img/empate.png'
-    };
-    if (pcTiro=='Tijera') {
-        imgPc.src = './img/tijera.png'
-        console.log('Tú pierdes'); 
-        resultado.src = './img/perder.png'
-    };
-
+    playerTiro= 'papel'
 
 });
 
@@ -83,23 +59,41 @@ btnTijera.addEventListener('click', function(){
     imgJugador.src = './img/tijera.png'
     divJugador.style.display= 'block';
     boton.disabled= false;
-
-    if (pcTiro== 'Piedra') {
-        imgPc.src = './img/piedra.png'
-        console.log('Tú pierdes'); 
-        resultado.src = './img/perder.png'
-    };
-    if (pcTiro=='Papel') {
-        imgPc.src = './img/papel.png'
-        console.log('Tú ganas'); 
-        resultado.src = './img/ganar.png'
-    };
-    if (pcTiro=='Tijera') {
-        imgPc.src = './img/tijera.png'
-        console.log('Empate'); 
-        resultado.src = './img/empate.png'
-    };
+    playerTiro= 'tijera'
 });
+
+function jugar(){
+    if(playerTiro==pcTiro){
+        console.log('Empate');
+        imgPc.src = `./img/${pcTiro}.png`;
+        resultado.src = './img/empate.png'
+        respuesta= 'Empate'
+    } else if(pcTiro=='piedra' && playerTiro=='papel' || pcTiro=='tijera' && playerTiro=='piedra' || pcTiro=='papel' && playerTiro=='tijera'){
+        console.log('Tú ganas');
+        imgPc.src = `./img/${pcTiro}.png`;
+        resultado.src = './img/ganar.png';
+        respuesta= 'Ganaste'
+    } else {
+        console.log('Tú pierdes')
+        imgPc.src = `./img/${pcTiro}.png`;
+        resultado.src = './img/perder.png';
+        respuesta= 'Perdiste'
+    }
+    resultadosHistorial.push(respuesta); // Agregar resultado al historial
+    sessionStorage.setItem('historialDePartidas', JSON.stringify(resultadosHistorial)); // Actualizar sessionStorage
+
+}
+
+function imprimirHistorial(){
+    resultadosHistorial.forEach((respuesta, index)=>{
+        elemento=   `   <li class="partida-elemento">
+        <span>Partida número:</span> ${index+1}
+        <span>El resultado es: </span> ${respuesta}
+        </li> `
+        listaHistorial.insertAdjacentHTML("beforeend", elemento)
+
+    })
+}
 
 boton.addEventListener('click', function(){
         respuestas.forEach(function(div) {
@@ -111,14 +105,44 @@ boton.addEventListener('click', function(){
         btnTijera.disabled= true;
 
         instruccion.style.display='none'
-
+        jugar()
     });
 
 reload.addEventListener('click', function(){
     location.reload();
 });
 
+
+// modal
+// Obtener elementos del DOM
+const openModalBtn = document.getElementById('openModalBtn');
+const modal = document.getElementById('modal');
+const closeModal = document.getElementById('closeModal');
+
+
+// Abrir el modal al hacer clic en el botón
+openModalBtn.addEventListener('click', ()=> {
+  modal.style.display = 'block';
+  imprimirHistorial()
+
 });
 
-               
-               
+// Cerrar el modal al hacer clic en la "X"
+closeModal.addEventListener('click', function() {
+  modal.style.display = 'none';
+});
+
+// Cerrar el modal al hacer clic fuera del contenido del modal
+window.addEventListener('click', function(event) {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+});
+
+let data = sessionStorage.getItem('historialDePartidas')
+if (data){
+    resultadosHistorial= JSON.parse(data)
+} else{
+    resultadosHistorial=[]
+}
+
